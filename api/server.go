751ff92445490787e66aa358a2610487c0e4985e
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-09-13 15:02:52 -0400

    build out api code

---
+++ api/server.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// StartServer creates a new server instance
+func StartServer() *gin.Engine {
+
+	fmt.Printf("Starting server on port :%s\n", os.Getenv("PORT"))
+	// This should not be hardcoded. It should be set in the environment but for some reason it is not working
+	if os.Getenv("GIN_MODE") == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	router := gin.Default()
+	// Pass routes to the router
+	// RoutesRecipe(router)
+	router.Run()
+	return router
+}
