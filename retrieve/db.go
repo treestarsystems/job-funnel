d4332b8f98d4b6d253dcd322ac64932fbb2efb8d
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-09-21 15:08:00 -0400

    adding/refactoring db code for load and retrieve packages

--- retrieve/db.go
+++ retrieve/db.go
@@ -8,10 +8,10 @@ import (
 func RetrieveDbDataAll() []utils.JobPost {
 
 	// Upsert job posts to the database
-	if os.Getenv("DB_MONGODB_ENABLE") == "true" {
-		// loadDbDataToMongoDb(data, jobId)
-		return []utils.JobPost{}
-	}
+	// if os.Getenv("DB_MONGODB_ENABLE") == "true" {
+	// 	// loadDbDataToMongoDb(data, jobId)
+	// 	return []utils.JobPost{}
+	// }
 
 	if os.Getenv("DB_SQLITE_ENABLE") == "true" {
 		return RetrieveDbFromSqliteAll()
