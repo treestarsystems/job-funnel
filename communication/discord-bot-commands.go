20f62b39ff4bb2cf870cccf7510783a367b66465
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-09-20 20:35:41 -0400

    refactor bot commands to separate file

---
+++ communication/discord-bot-commands.go
@@ -0,0 +1,90 @@
+package communication
+
+import (
+	"fmt"
+	"job-funnel/retrieve"
+	"job-funnel/utils"
+	"regexp"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func discordBotSlashCommandHelpMenu() string {
+	helpMenu := []string{
+		"Commands:",
+		"!job:all - Get all job posts (see notes below)",
+		"!job:random - Get a random single job post",
+		"!job:search <search term as single word no spaces> - Get all job posts that match the search string (see notes below)",
+		"!applied:<job ID> - Mark a job post as applied so it is not shown in future searches (not available yet)",
+		"!applied:all - Get all job posts that have been marked as applied (not available yet)",
+		"!help - Get a list of commands",
+		"\nNote: \n- Due to [message length limitations of 2k characters](https://support.discord.com/hc/en-us/articles/360034632292-Sending-Messages#h_01FSWZRDKD7310TJHGCA616642:~:text=place%20multiple%20emojis%3A-,Character%20Limit,-The%20character%20cap), I can not return all stored job posts. \n- If you multiple job posts will be returned, the order will be random so you can get different jobs. \n- This bot is still in development.",
+	}
+	return strings.Join(helpMenu, "\n")
+}
+
+// sendAllJobPosts retrieves all job posts and sends them as a response.
+func sendAllJobPosts(session *discordgo.Session, channelID string) {
+	resultJobPosts := retrieve.RetrieveDataFromSqliteAll()
+	if len(resultJobPosts) == 0 {
+		session.ChannelMessageSend(channelID, "Sorry, no job posts available.")
+		return
+	}
+	jobPostsResponse := utils.JobPostsToString(resultJobPosts)
+	session.ChannelMessageSend(channelID, jobPostsResponse)
+}
+
+// sendRandomJobPost retrieves all job posts, selects a random one, and sends it as a response.
+func sendRandomJobPost(session *discordgo.Session, channelID string) {
+	resultJobPosts := retrieve.RetrieveDataFromSqliteAll()
+	if len(resultJobPosts) == 0 {
+		session.ChannelMessageSend(channelID, "Sorry, no job posts available.")
+		return
+	}
+	jobPostsResponse := utils.JobPostsToStringSingle(resultJobPosts)
+	session.ChannelMessageSend(channelID, jobPostsResponse)
+}
+
+// searchAndSendJobPosts searches for job posts based on the search term and sends the response.
+func searchAndSendJobPosts(session *discordgo.Session, channelID string, searchTerm string) {
+	resultJobPosts := retrieve.RetrieveDataFromSqliteAll()
+	var filteredJobPosts []utils.JobPost
+	for _, job := range resultJobPosts {
+		if strings.Contains(strings.ToLower(job.JobTitle), strings.ToLower(searchTerm)) ||
+			strings.Contains(strings.ToLower(job.Description), strings.ToLower(searchTerm)) ||
+			strings.Contains(strings.ToLower(strings.Join(job.CodingLanguage, " ")), strings.ToLower(searchTerm)) ||
+			strings.Contains(strings.ToLower(strings.Join(job.CodingFramework, " ")), strings.ToLower(searchTerm)) ||
+			strings.Contains(strings.ToLower(strings.Join(job.Database, " ")), strings.ToLower(searchTerm)) ||
+			strings.Contains(strings.ToLower(strings.Join(job.WorkLocation, " ")), strings.ToLower(searchTerm)) {
+			filteredJobPosts = append(filteredJobPosts, job)
+		}
+	}
+	if len(filteredJobPosts) == 0 {
+		session.ChannelMessageSend(channelID, "Sorry, no job posts found matching the search term.")
+		return
+	}
+	jobPostsResponse := utils.JobPostsToString(filteredJobPosts)
+	session.ChannelMessageSend(channelID, jobPostsResponse)
+}
+
+func discordBotSlashCommands(session *discordgo.Session, message *discordgo.MessageCreate) {
+	appliedRegex := regexp.MustCompile(`^!applied:[a-zA-Z0-9]{20}$`)
+
+	switch {
+	case message.Content == "!help":
+		session.ChannelMessageSend(message.ChannelID, discordBotSlashCommandHelpMenu())
+	case message.Content == "!job:all":
+		sendAllJobPosts(session, message.ChannelID)
+	case message.Content == "!job:random":
+		sendRandomJobPost(session, message.ChannelID)
+	case message.Content == "!job:search ":
+		searchTerm := strings.TrimPrefix(message.Content, "!job:search ")
+		searchAndSendJobPosts(session, message.ChannelID, searchTerm)
+	case message.Content == "!applied:all":
+		session.ChannelMessageSend(message.ChannelID, "Feature not available yet.")
+	case appliedRegex.MatchString(message.Content):
+		jobId := strings.Split(message.Content, ":")[1]
+		session.ChannelMessageSend(message.ChannelID, fmt.Sprintf("Feature not available yet. ID: %s", jobId))
+	}
+}
--- communication/discord-bot.go
+++ communication/discord-bot.go
@@ -2,10 +2,7 @@ package communication
 
 import (
 	"fmt"
-	"job-funnel/retrieve"
-	"job-funnel/utils"
 	"os"
-	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -22,64 +19,15 @@ func discordBotReadConfig() error {
 	return nil
 }
 
+// MessageHandler handles messages sent to the Discord bot
 func discordBotMessageHandler(session *discordgo.Session, message *discordgo.MessageCreate) {
 	if message.Author.ID == discordBotId {
 		return
 	}
-	if message.Content == "!help" {
-		helpMenu := []string{
-			"Commands:",
-			"!job:all - Get all job posts (see notes below)",
-			"!job:random - Get a random single job post",
-			"!job:search <search term as single word no spaces> - Get all job posts that match the search string (see notes below)",
-			"!applied:<job ID> - Mark a job post as applied so it is not shown in future searches (not available yet)",
-			"!applied:all - Get all job posts that have been marked as applied (not available yet)",
-			"!help - Get a list of commands",
-			"\nNote: \n- Due to [message length limitations of 2k characters](https://support.discord.com/hc/en-us/articles/360034632292-Sending-Messages#h_01FSWZRDKD7310TJHGCA616642:~:text=place%20multiple%20emojis%3A-,Character%20Limit,-The%20character%20cap), I can not return all stored job posts. \n- If you multiple job posts will be returned, the order will be random so you can get different jobs. \n- This bot is still in development.",
-		}
-		session.ChannelMessageSend(message.ChannelID, strings.Join(helpMenu, "\n"))
-	}
-	if message.Content == "!job:all" {
-		resultJobPosts := retrieve.RetrieveDataFromSqliteAll()
-		if len(resultJobPosts) == 0 {
-			session.ChannelMessageSend(message.ChannelID, "Sorry, no job posts available.")
-		}
-		jobPostsResponse := utils.JobPostsToString(resultJobPosts)
-		session.ChannelMessageSend(message.ChannelID, jobPostsResponse)
-		// session.ChannelMessageSend(message.ChannelID, "Sorry no job posts available.")
-	}
-	if message.Content == "!job:random" {
-		resultJobPosts := retrieve.RetrieveDataFromSqliteAll()
-		if len(resultJobPosts) == 0 {
-			session.ChannelMessageSend(message.ChannelID, "Sorry, no job posts available.")
-		}
-		jobPostsResponse := utils.JobPostsToStringSingle(resultJobPosts)
-		session.ChannelMessageSend(message.ChannelID, jobPostsResponse)
-	}
-	if strings.HasPrefix(message.Content, "!job:search ") {
-		searchTerm := strings.TrimPrefix(message.Content, "!job:search ")
-		// Need new method to retrieve data from sqlite using search term.
-		resultJobPosts := retrieve.RetrieveDataFromSqliteAll()
-		var filteredJobPosts []utils.JobPost
-		for _, job := range resultJobPosts {
-			if strings.Contains(strings.ToLower(job.JobTitle), strings.ToLower(searchTerm)) ||
-				strings.Contains(strings.ToLower(job.Description), strings.ToLower(searchTerm)) ||
-				strings.Contains(strings.ToLower(strings.Join(job.CodingLanguage, " ")), strings.ToLower(searchTerm)) ||
-				strings.Contains(strings.ToLower(strings.Join(job.CodingFramework, " ")), strings.ToLower(searchTerm)) ||
-				strings.Contains(strings.ToLower(strings.Join(job.Database, " ")), strings.ToLower(searchTerm)) ||
-				strings.Contains(strings.ToLower(strings.Join(job.WorkLocation, " ")), strings.ToLower(searchTerm)) {
-				filteredJobPosts = append(filteredJobPosts, job)
-			}
-		}
-		if len(filteredJobPosts) == 0 {
-			session.ChannelMessageSend(message.ChannelID, "Sorry, no job posts found matching the search term.")
-			return
-		}
-		jobPostsResponse := utils.JobPostsToString(filteredJobPosts)
-		session.ChannelMessageSend(message.ChannelID, jobPostsResponse)
-	}
+	discordBotSlashCommands(session, message)
 }
 
+// InitDiscordBot initializes the Discord bot
 func InitDiscordBot() {
 	discordBotReadConfig()
 	discordBotSession, err := discordgo.New("Bot " + discordBotToken)
